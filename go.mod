513bd111463abd8f418777ee70197398f7ba612e
Aaron Boodman <aaron@aaronboodman.com>
2019-02-09 00:49:29 -0800

    wip

--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github.com/tythe-protocol/z_test1
+
+require github.com/tythe-protocol/z_test2 v0.0.0-20190209084623-734d615d6b1c
--- lib.go
+++ lib.go
@@ -2,8 +2,10 @@ package z_test1
 
 import (
 	"github.com/tythe-protocol/z_test1/dep"
+	"github.com/tythe-protocol/z_test2"
 )
 
 func Greet() {
 	dep.Hello()
+	z_test2.Yo()
 }
